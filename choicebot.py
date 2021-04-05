d48c49c48120871e7fca7b56d840a7c3173900d3
Subfizz <58484034+Subfizz@users.noreply.github.com>
2021-04-05 22:45:38 +0300

    Choicebot
    Version 1.0

---
+++ choicebot.py
@@ -0,0 +1,35 @@
+import telebot
+import random
+USERS = []
+
+bot = telebot.TeleBot('1576472189:AAFj9CtbyXHw31KIJeiXWtxNYQD80uH6wIc');
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+	if message.chat.id not in USERS:
+		bot.send_message(message.chat.id, 'Привет! Это бот, который поможет тебе определиться с выбором. Подумай, какое действие или предмет относится к какой цифре и выбери количество исходов! Что выпало, то и выбирай! Пиши /random', parse_mode='html')
+		USERS.append(message.chat.id)
+	else:
+		bot.send_message(message.chat.id, 'Извини, ты уже его активировал')
+
+
+@bot.message_handler(commands=['random'])
+def start(message):
+	bot.send_message(message.chat.id,'Количество исходов?')
+	bot.register_next_step_handler(message, get_login)
+
+def get_login(message):
+	global iscount
+	iscount = message.text
+	ran = ''.join(iscount)
+	try:
+		randumb = random.randint(int(1), int(ran))
+		bot.send_message(message.chat.id,randumb)
+	except ValueError:
+		bot.send_message(message.chat.id,'не число')
+	bot.register_next_step_handler(message, get_login)
+
+
+if __name__ == '__main__':
+	bot.polling(none_stop=True)
